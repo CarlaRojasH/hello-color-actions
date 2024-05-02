af512159f28e6d63160085187ac23bbf8e5cfdf8
CarlaRojasH <you@example.com>
2024-05-01 22:30:08 -0500

    "Hola, mundo, desde Lima Peru!"

--- hello.test.js
+++ hello.test.js
@@ -1,7 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("const hello = () => "Hola, mundo, desde Lima Peru!";
-    !");
+    expect(hello.hello()).toEqual("Hola, mundo, desde Lima Peru!");
   });
 });
